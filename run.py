a4946c455e817379cd49e0731e4bce4a5a93e2c9
blahblahblah589 <hwright589@icloud.com>
2023-04-17 17:58:26 +0000

    create functions to retrive and validate user input

--- run.py
+++ run.py
@@ -8,6 +8,27 @@ SCOPE = [
     ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(scope)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('monthly-budget')
+
+def get_income():
+    """
+    Get take home income for this month from user
+    """
+    print("How much were you paid after tax this month?\n")
+
+    income = input("Enter figure here:\n")
+
+    if validate_figures(income):
+        print("Thank you")
+    
+    return income
+
+def validate_figures(value):
+    """
+    Ensure all inputs are provided as integers, 
+    provide error if invalid
+    """
+    if isinstance(value, int):
+        return True
