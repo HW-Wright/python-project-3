9135a37f52fd7e36294916b1eba9f547c54b77ad
blahblahblah589 <hwright589@icloud.com>
2023-04-18 15:59:13 +0000

    create function to retrive data from user, validate said data and store in spreadsheet

--- run.py
+++ run.py
@@ -14,21 +14,72 @@ SHEET = GSPREAD_CLIENT.open('monthly-budget')
 
 def get_income():
     """
-    Get take home income for this month from user
+    Get take home income for this month from user.
     """
     print("How much were you paid after tax this month?\n")
 
-    income = input("Enter figure here:\n")
+    income = int(input("Enter figure here:\n"))
 
-    if validate_figures(income):
-        print("Thank you")
+    if validate_income(income):
+        print("Thank you.\n")
     
     return income
 
-def validate_figures(value):
+
+# def get_living_costs():
+#     """
+#     Get the total living costs (rent, energy,
+#     groceries and council tax) as a series of integers
+#     separated by a comma
+#     """
+#     print("Please enter living costs for this month.\n")
+#     print("In order of: Rent->Energy->Groceries->Council Tax\nEmter data as integers separated by a comma")
+    
+#     living_costs = input("Enter figures here:\n")
+
+#     if validate_living(living_costs):
+#         print("Thank you")
+#         break
+
+#     return living_costs
+
+
+def validate_income(value):
     """
-    Ensure all inputs are provided as integers, 
-    provide error if invalid
+    Ensure income data is provided as integers, 
+    provide error if invalid.
     """
     if isinstance(value, int):
         return True
+
+
+# def validate_living(value):
+#     """
+#     Convert data into integers
+#     """
+    
+
+
+def update_budget(data, worksheet):
+    """
+    Used to update the spreadsheet with input data
+    from user, in all cases.
+    """
+    print(f"Updating {worksheet} budget...\n")
+    data = [data]
+    updating = SHEET.worksheet(worksheet)
+    updating.append_row(data)
+    print("Updated.\n")
+
+
+def run_functions():
+    """
+    Function to run all other functions.
+    """
+    income = get_income()
+    update_budget(income, "Income")
+    # living_costs = get_living_costs()
+    # update_budget(living_costs, "Living")
+
+
+run_functions()
